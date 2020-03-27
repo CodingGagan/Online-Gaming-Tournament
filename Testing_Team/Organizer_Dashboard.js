04dab77ac57fbf50e86e8086adafec9f79fa9613
learning4php <learning4php@gmail.com>
2020-03-27 13:12:53 +0530

    add organizer and admin folder and some pages

--- Testing_Team/Organizer_Dashboard.js
+++ Testing_Team/Organizer_Dashboard.js
@@ -25,105 +25,105 @@ window.onclick = (event) => {
 
 
 // CHART SCRIPT
-var config = {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'Unfilled',
-            fill: false,
-            backgroundColor: window.chartColors.red,
-            borderColor: window.chartColors.red,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-        }, {
-            label: 'Dashed',
-            fill: false,
-            backgroundColor: window.chartColors.grey,
-            borderColor: window.chartColors.grey,
-            borderDash: [5, 5],
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-        }, {
-            label: 'Filled',
-            backgroundColor: window.chartColors.orange,
-            borderColor: window.chartColors.orange,
-            data: [
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor(),
-                randomScalingFactor()
-            ],
-            fill: true,
-        }]
-    },
-    options: {
-        responsive: true,
-        // title: {
-        //     display: true,
-        //     text: 'Total Profit'
-        // },
-        tooltips: {
-            mode: 'index',
-            intersect: false,
-        },
-        hover: {
-            mode: 'nearest',
-            intersect: true
-        },
-        scales: {
-            xAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Month'
-                }
-            }],
-            yAxes: [{
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Value'
-                }
-            }]
-        }
-    }
-};
+// var config = {
+//     type: 'line',
+//     data: {
+//         labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//         datasets: [{
+//             label: 'Unfilled',
+//             fill: false,
+//             backgroundColor: window.chartColors.red,
+//             borderColor: window.chartColors.red,
+//             data: [
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor()
+//             ],
+//         }, {
+//             label: 'Dashed',
+//             fill: false,
+//             backgroundColor: window.chartColors.grey,
+//             borderColor: window.chartColors.grey,
+//             borderDash: [5, 5],
+//             data: [
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor()
+//             ],
+//         }, {
+//             label: 'Filled',
+//             backgroundColor: window.chartColors.orange,
+//             borderColor: window.chartColors.orange,
+//             data: [
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor(),
+//                 randomScalingFactor()
+//             ],
+//             fill: true,
+//         }]
+//     },
+//     options: {
+//         responsive: true,
+//         // title: {
+//         //     display: true,
+//         //     text: 'Total Profit'
+//         // },
+//         tooltips: {
+//             mode: 'index',
+//             intersect: false,
+//         },
+//         hover: {
+//             mode: 'nearest',
+//             intersect: true
+//         },
+//         scales: {
+//             xAxes: [{
+//                 display: true,
+//                 scaleLabel: {
+//                     display: true,
+//                     labelString: 'Month'
+//                 }
+//             }],
+//             yAxes: [{
+//                 display: true,
+//                 scaleLabel: {
+//                     display: true,
+//                     labelString: 'Value'
+//                 }
+//             }]
+//         }
+//     }
+// };
 
-window.onload = function() {
-    var ctx = document.getElementById('canvas').getContext('2d');
-    window.myLine = new Chart(ctx, config);
-};
+// window.onload = function() {
+//     var ctx = document.getElementById('canvas').getContext('2d');
+//     window.myLine = new Chart(ctx, config);
+// };
 
 
 
-function NotificationIcon() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-  }
-function sendMail(){
-    var popup = document.getElementById("myPopup2");
-    popup.classList.toggle("show2");
-  }
+// function NotificationIcon() {
+//     var popup = document.getElementById("myPopup");
+//     popup.classList.toggle("show");
+//   }
+// function sendMail(){
+//     var popup = document.getElementById("myPopup2");
+//     popup.classList.toggle("show2");
+//   }
 
-function WhatsNew() {
-    var popup = document.getElementById("myPopup3");
-    popup.classList.toggle("show3");
-  }
+// function WhatsNew() {
+//     var popup = document.getElementById("myPopup3");
+//     popup.classList.toggle("show3");
+//   }
